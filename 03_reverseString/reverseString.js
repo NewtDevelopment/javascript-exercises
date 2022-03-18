75e0aea365c066f8ef3cb0178ca5f07c0c69f629
Jeremy Driesler <jeremy@newt.dev>
2022-03-18 13:08:18 -0400

    Complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let letterArray = [];
+    let reversed = "";
+    for(const letter of input){
+        letterArray.push(letter);
+    }
+    let letterArrayInitialLength = letterArray.length;
+    for(let i = 0; i <letterArrayInitialLength; i++){
+        reversed += letterArray.pop()
+    }
 
+    return reversed;
 };
 
 // Do not edit below this line
