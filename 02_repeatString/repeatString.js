3d9ae2221cf2c6a534c85dd97c03d7615b3f5830
Jeremy Driesler <jeremy@newt.dev>
2022-03-18 12:55:04 -0400

    Complete exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(sent, repititions) {
+    if (repititions <0){
+        return "ERROR"
+    }
+    let phrase = "";
+    for(let i = 0; i < repititions; i++){
+        phrase = phrase + sent;
+    }
+    return phrase;
 };
 
 // Do not edit below this line
