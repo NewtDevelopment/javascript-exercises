28c57b0c249c6678ef30d0ec3a5911dbe9f0381d
Jeremy Driesler <jeremy@newt.dev>
2022-03-18 13:51:21 -0400

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(in1, in2) {
+    if (typeof(in1) != "number" || typeof(in2) != "number"){
+        return "ERROR";
+    }
+    if(in1 < 0||in2<0){
+        return "ERROR";
+    }
+    if(in2 < in1){
+        console.log("in2 less than in1")
+        let transitionNumber = in1;
+        in1 = in2;
+        in2 = transitionNumber;
+    }
+    let sum = 0;
+    for(let i = in1; i <=in2;i++ ){
+        sum = sum + i;
+    }
+    return sum;
 
 };
 
