661de49385d9debfa8618dc87b2b9abf185b3b0a
Jeremy Driesler <jeremy@newt.dev>
2022-03-18 13:37:59 -0400

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, in1, in2, in3, in4) {
+    let initArrayLength = inputArray.length;
+    let initRemoveArray = [];
+    initRemoveArray.push(in1);
+    initRemoveArray.push(in2);
+    initRemoveArray.push(in3);
+    initRemoveArray.push(in4);
+
+    let index = 0;
+    for (let i = 0; i < initArrayLength; i++){
+        if(initRemoveArray.includes(inputArray[i])){
+            inputArray.splice(i, 1);
+            console.log(`Spliced a thing ${i}, ${inputArray[i]}`);
+            i--;
+            initArrayLength--;
+
+        }      
+        
+    }
+    return inputArray;
 
 };
 
